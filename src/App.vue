c147dab6c8bd7c9fc17efba8a305d4f915115200
mohmaedelaraby <elarabym857@gmail.com>
2022-06-12 11:15:54 +0200

    try #

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
 <div class="page">
- <h1>Task Load on Scroll</h1>
  <section v-for="(item,index) in list_of_data" v-bind:key="index" class="card">
  <h2>{{item.title}}</h2>
  <p>{{item.body}}</p>
- 
  </section>
 </div>
 
@@ -30,16 +28,64 @@ export default {
 ,
  updated(){
    const cards= document.querySelectorAll(".card");
+   const lastC = document.querySelectorAll(".card:last-child")
 
       const observer = new IntersectionObserver(entries=>{
         entries.forEach(entry=>{
           entry.target.classList.toggle("show",entry.isIntersecting)
+          console.log("lc",lastC)
+          if(entry.isIntersecting){
+            //load just once
+            observer.unobserve(entry.target)
+          }
         })
+      },{
+        //load if 100% od section is intersect
+        threshold:1
       });
+
+      
+      const lastObserver = new IntersectionObserver ( entries=>{
+        const lastcard =entries[0];
+        if(!lastcard.isIntersecting) 
+        {
+          console.log("LAst card intersect")
+          return
+          }
+          console.log("lastCArd Not Intersect")
+        loadNewCards();
+        lastObserver.unobserve(lastcard.target);
+        
+      },{
+        root:null,
+        threshold:0
+      })
+
+      //lastObserver.observe(lastC)
+
+
+      const cardContainer = document.querySelector('.page')
+
+      const loadNewCards =()=>{
+        for(let i=0;i<10;i++){
+          const card =document.createElement('section');
+          const header= document.createElement("h2")
+          const parag= document.createElement("p")
+          const contentHeader = document.createTextNode("HI I'M HEADER")
+          const contentprg = document.createTextNode("HI I'M paragarpgh")
+          header.appendChild(contentHeader)
+          parag.appendChild(contentprg)
+          card.appendChild(header,parag);
+          console.log("NEWCARD",card)
+          card.classList.add("card")
+          observer.observe(card)
+         cardContainer.appendChild(card)
+        }
+      }
 
       cards.forEach(card=>{
         observer.observe(card);
-        console.log("Card :",card);
+    
       })
  }
 
@@ -57,8 +103,6 @@ h1{
   margin-top: 50px;
 }
 .page{
-  height: 100vh;
- 
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -74,7 +118,7 @@ h1{
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  transition: 1500ms;
+  transition: 800ms;
   opacity: 0;
 }
 .card.show{
